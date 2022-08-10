6410810bfb7b81f20b44ea1382495b15cb6d7936
Martina Rossini <martina.rossini704@gmail.com>
2022-08-10 16:00:54 +0200

    Added wandb callback

---
+++ src/callbacks/wandb.py
@@ -0,0 +1,47 @@
+from abc import ABC
+import wandb
+
+class RLCallback(ABC):
+    def __init__(self):
+        super().__init__()
+    
+    def on_train_begin(self, logs=None):
+        return logs
+    
+    def on_eval_begin(self, logs=None):
+        return logs
+    
+    def on_train_episode_end(self, logs=None):
+        return logs
+    
+    def on_eval_episode_begin(self, logs=None):
+        return logs
+
+    def on_train_end(self, logs=None):
+        return logs
+    
+    def on_eval_end(self, logs=None):
+        return logs
+
+class WandbCallback(RLCallback):
+    def __init__(self, project, name, group, config):
+        super().__init__()
+        self.run = wandb.init(
+            project=project,
+            name=name,
+            group=group,
+            reinit=True,
+            config=config
+        )
+    
+    def on_train_episode_end(self, logs=None):
+        episode = logs['episode']
+        logs_to_wandb = {key: value for key, value in logs.items() if key != 'episode' and value is not None}
+        for score_name, score_value in logs_to_wandb.items():
+            wandb.log({score_name: score_value, 'episode': episode}, step=episode)
+        return logs
+
+    def on_eval_end(self, logs=None):
+        self.run.finish()
+        return logs
+
